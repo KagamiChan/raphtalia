42261fa07446a4ba3c55d1ca1b633beca1c8f12e
Kagami <moe.pascal@gmail.com>
2019-03-22 01:22:14 +0800

    test: some server tests

--- src/__tests__/requests.spec.ts
+++ src/__tests__/requests.spec.ts
@@ -2,15 +2,37 @@ import request from 'supertest'
 import glob from 'glob'
 import { each } from 'lodash'
 import path from 'path'
+import Bluebird from 'bluebird'
+import faker from 'faker'
 
 import { buildFastify } from '../app'
 
+const fastify = buildFastify()
+
+beforeAll(() => fastify.ready())
+afterAll(() => fastify.close())
+
+describe('server', () => {
+  it('returns 404 for other methods', async done => {
+    await Bluebird.each(['get', 'put', 'head', 'delete', 'options'], method =>
+      request(fastify.server)
+        [method as 'get']('/webhook')
+        .expect(404),
+    )
+
+    done()
+  })
+
+  it('returns 404 for other endpoints', async done => {
+    await request(fastify.server)
+      .post(`/${faker.random.word()}`)
+      .expect(404)
+
+    done()
+  })
+})
+
 describe('webhook events', () => {
-  const fastify = buildFastify()
-
-  beforeAll(() => fastify.ready())
-  afterAll(() => fastify.close())
-
   const events = glob.sync(path.resolve(__dirname, './events/*.json'))
 
   each(events, event => {
